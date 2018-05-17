f6cc57d276c53167958a2ba71202e1ea9337664e
Jason Lim <jihoon2514@gmail.com>
2018-05-17 04:33:16 -0400

    all blogs done

--- blog/models.py
+++ blog/models.py
@@ -10,22 +10,26 @@ class Blog(models.Model):
   title = models.CharField(max_length=255)
   pub_date = models.DateTimeField()
   body = models.TextField()
-  image = models.ImageField(upload_to='images/')
+  image = models.ImageField(upload_to='images/', blank=True)
 
   def __str__(self):
     return self.title
 
   def summary(self):
-    return self.body[:100]
+    return self.first_words(self.body, 30)
+
+  def first_words(self, input, words):
+    for i in range(0, len(input)):
+        # Count spaces in the string.
+        if input[i] == ' ':
+            words -= 1
+        if words == 0:
+            # Return the slice up to this point.
+            return input[0:i] + '...'
+    return input
 
   def pub_date_pretty(self):
-    return self.pub_date.strftime('%b %e %Y')
-
+    return self.pub_date.strftime('%y.%m.%d')
   
-# Add the Blog app to the settings
-
-# Create a migration
-
-# Migrate
-
-# Add to the admin
+  class Meta:
+    ordering = ["-pub_date"]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,20 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
+# def allblogs(request):
+#   blogs = Blog.objects.all()
+#   paginator = Paginator(blogs, 5) # Show 25 contacts per page
+#   return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
 def allblogs(request):
-  blogs = Blog.objects
-  return render(request, 'blog/allblogs.html', {'blogs': blogs})
+    blog_list = Blog.objects.all()
+    paginator = Paginator(blog_list, 3) # Show 5 blogs per page
+
+    page = request.GET.get('page')
+    blogs = paginator.get_page(page)
+    return render(request, 'blog/allblogs.html', {'blogs': blogs})
 
 def blogdetail(request, blog_id):
   detailblog = get_object_or_404(Blog, pk=blog_id)
