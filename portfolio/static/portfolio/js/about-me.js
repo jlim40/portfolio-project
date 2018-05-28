5ca755b9a2470316ef47af47e586dc996b4eaef6
Jason Lim <jihoon2514@gmail.com>
2018-05-27 21:08:00 -0400

    Vertical menu finished

--- portfolio/static/portfolio/js/about-me.js
+++ portfolio/static/portfolio/js/about-me.js
@@ -1,7 +1,7 @@
 new TypeIt('#type-it-1', {
   strings:
     '<span class="type-it-blue pr-2">const</span> aboutMe<span class="type-it-red px-2"> = </span>{',
-  speed: 80,
+  speed: 40,
   autoStart: false,
   afterComplete: function() {
     $('#type-it-1')
@@ -10,7 +10,7 @@ new TypeIt('#type-it-1', {
 
     new TypeIt('#type-it-2', {
       strings: 'name: <span class="type-it-yellow pl-2"> \'Jason\'</span>,',
-      speed: 80,
+      speed: 40,
       autoStart: false,
       afterComplete: function() {
         $('#type-it-2')
@@ -20,7 +20,7 @@ new TypeIt('#type-it-1', {
         new TypeIt('#type-it-3', {
           strings:
             'location: <span class="type-it-yellow pl-2"> \'Toronto\'</span>,',
-          speed: 80,
+          speed: 40,
           autoStart: false,
           afterComplete: function() {
             $('#type-it-3')
@@ -29,7 +29,7 @@ new TypeIt('#type-it-1', {
 
             new TypeIt('#type-it-4', {
               strings: 'roles: [',
-              speed: 80,
+              speed: 40,
               autoStart: false,
               afterComplete: function() {
                 $('#type-it-4')
@@ -37,7 +37,7 @@ new TypeIt('#type-it-1', {
                   .css('display', 'none');
 
                 new TypeIt('#type-it-5', {
-                  speed: 80,
+                  speed: 40,
                   autoStart: false,
                   afterComplete: function() {
                     $('#type-it-5')
@@ -47,7 +47,7 @@ new TypeIt('#type-it-1', {
                     new TypeIt('#type-it-6', {
                       strings:
                         '<span class="type-it-yellow">\'Javascript Developer\'</span>',
-                      speed: 80,
+                      speed: 40,
                       autoStart: false,
                       afterComplete: function() {
                         $('#type-it-6')
@@ -56,7 +56,7 @@ new TypeIt('#type-it-1', {
 
                         new TypeIt('#type-it-7', {
                           strings: '],',
-                          speed: 80,
+                          speed: 40,
                           autoStart: false,
                           afterComplete: function() {
                             $('#type-it-7')
@@ -66,7 +66,7 @@ new TypeIt('#type-it-1', {
                             new TypeIt('#type-it-8', {
                               strings:
                                 'hobby: <span class="type-it-yellow pl-2"> \'Drinking Espresso\'</span>,',
-                              speed: 80,
+                              speed: 40,
                               autoStart: false,
                               afterComplete: function() {
                                 $('#type-it-8')
@@ -99,7 +99,7 @@ new TypeIt('#type-it-1', {
                   .options({ speed: 100 })
                   .delete()
                   .pause(300)
-                  .options({ speed: 80 })
+                  .options({ speed: 40 })
                   .type("'Junior Web Developer',")
                   .pause(400);
               }
