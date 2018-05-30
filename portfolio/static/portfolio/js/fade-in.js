142b607058d609e0dd4c8f8a55c63114b84b1446
Jason Lim <jihoon2514@gmail.com>
2018-05-30 00:49:24 -0400

    Start from scratch

--- portfolio/static/portfolio/js/fade-in.js
+++ portfolio/static/portfolio/js/fade-in.js
@@ -1,18 +1,36 @@
+// $(document).ready(function() {
+//   /* Every time the window is scrolled ... */
+//   $(window).scroll(function() {
+//     /* Check the location of each desired element */
+//     let delay_counter = 0;
+//     $('.portfolio-hideme').each(function(i) {
+//       var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+//       var bottom_of_window = $(window).scrollTop() + $(window).height();
+//       /* If the object is completely visible in the window, fade it it */
+//       if (bottom_of_window > bottom_of_object) {
+//         $(this)
+//           .delay(delay_counter)
+//           .animate({ opacity: 1 }, 1000);
+//       }
+//       delay_counter += 170;
+//     });
+//   });
+
+//   // Delete link for portfolo job
+//   $('a[href*="jasonlim.info/#portfolio"]').removeAttr('target');
+// });
+
 $(document).ready(function() {
-  /* Every time the window is scrolled ... */
   $(window).scroll(function() {
-    /* Check the location of each desired element */
-    let delay_counter = 0;
-    $('.hideme').each(function(i) {
+    $('.portfolio-hideme').each(function(i) {
       var bottom_of_object = $(this).offset().top + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
-      /* If the object is completely visible in the window, fade it it */
       if (bottom_of_window > bottom_of_object) {
-        $(this).animate({ opacity: '1' }, 1200);
+        $(this).addClass('showme');
+      }
+      if (bottom_of_window < bottom_of_object) {
+        $(this).removeClass('showme');
       }
     });
   });
-
-  // Delete link for portfolo job
-  $('a[href*="jasonlim.info/#portfolio"]').removeAttr('target');
 });
